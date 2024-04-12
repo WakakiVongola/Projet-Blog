12ebc144ae174b05e1a602236fd808abc12456e8
WakakiVongola <133748743+WakakiVongola@users.noreply.github.com>
2024-04-12 14:56:35 +0200

    Update header with inscription and connection

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,28 +1,98 @@
 'use client';
 import React, { useState } from 'react';
 import Image from 'next/image';
-
+import { Button } from "@/components/ui/button"
 import {
     Dialog,
     DialogClose,
     DialogContent,
     DialogDescription,
+    DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
   } from "@/components/ui/dialog"
+  import { Input } from "@/components/ui/input"
+  import { Label } from "@/components/ui/label"
+  import { Link } from 'lucide-react'
 
 
 export const Header = () => {
     return (
         <header className='bg-blanc'>
             <div className='flex justify-between items-center p-5'>
-            <Image
-                    src='/iconProfile.webp'
-                    alt="Picture of the author"
-                    width={100}
-                    height={100}
-                />
+            {/* Login */}
+                <Dialog>
+                    <DialogTrigger asChild>
+                    <Image
+                         src='/iconProfile.webp'
+                        alt="Picture of the author"
+                        width={100}
+                        height={100}
+                        />
+                    </DialogTrigger>
+                        <DialogContent className="sm:max-w-[425px] bg-blanc">
+                            <DialogHeader>
+                                <div className='border-b-2 border-gray-800'><img className='w-[30%]  mx-auto' src='/logo.png'/>
+                            <DialogTitle className='text-center p-2'>Connexion</DialogTitle></div>
+                            </DialogHeader>
+                            <div className="grid gap-4 py-4">
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="mail" className="text-right">
+                                    Mail
+                                    </Label>
+                                    <Input
+                                    id="mail"
+                                    className="col-span-3"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="Password" className="text-right">
+                                        Password
+                                    </Label>
+                                    <Input
+                                    id="Password"
+                                    className="col-span-3"
+                                    />
+                                </div>
+                            </div>
+                            <DialogFooter>
+                                <Button type="submit" className='hover:bg-marron'>Connexion</Button>   
+                                {/* Inscription */}
+                                <Dialog>
+                                    <DialogTrigger asChild>
+                                    <Button className='hover:bg-marron'>Créer un compte</Button>
+                                    </DialogTrigger>
+                                    <DialogContent className="sm:max-w-[425px] bg-blanc">
+                                        <DialogHeader>
+                                        <div className='border-b-2 border-gray-800'><img className='w-[30%]  mx-auto' src='/logo.png'/>
+                                        <DialogTitle className='text-center p-2'>Créer un Compte</DialogTitle></div>
+                                        </DialogHeader>
+                                    <div className='flex flex-col'>
+                                        <p className='mx-auto m-2'>Pseudo</p>
+                                        <input type='text' placeholder='Votre pseudo' className='mx-auto bg-white rounded-full p-2'/>
+                                    </div>
+                                    <div className='flex flex-col'>
+                                        <p className='mx-auto m-2'>Votre email</p>
+                                        <input type='text' placeholder='Email' className='mx-auto bg-white rounded-full p-2'/>
+                                    </div>
+                                    <div className='flex flex-col'>
+                                        <p className='mx-auto m-2'>Password</p>
+                                        <input type='password' placeholder='Votre mot de passe' className='mx-auto bg-white rounded-full p-2'/>
+                                    </div>
+                                    <div className="grid w-full max-w-sm items-center gap-1.5 justify-center">
+                                        <label htmlFor="picture" className="text-center">Ajouter une photo de profile</label>
+                                        <input id="picture" type="file"/>
+                                    </div>  
+                                        <div className='flex justify-center'>
+                                        <DialogClose><Button type="submit" className='mx-auto hover:bg-marron'>Créer</Button></DialogClose>
+                                        </div>
+                                    </DialogContent>
+                                </Dialog>
+                                
+                            </DialogFooter>
+                        </DialogContent>
+                    </Dialog>
                 <div id='TopNav' className='flex w-3/4 justify-between items-center'>
                     <a href="/accueil/villa">Villa</a>
                     <a href="https://twitter.com/?lang=fr">Maisson de campagne</a>
