8bba5318f324bcf9858bda9e2b0cf27153b62f77
Nathan <nathangoyer56@gmail.com>
2024-04-15 17:49:32 +0200

    détail

--- app/accueil/villa/page.tsx
+++ app/accueil/villa/page.tsx
@@ -1,8 +1,6 @@
 export default function Villa() {
     return (
-            <body className="bg-blanc">
-                <h1>Voici les villas</h1>
-            </body>
+            <h1>Voici les villas</h1>
     );
   }
   
