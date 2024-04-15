195c461361283d24923172c304a33e1b4a7a39c6
WakakiVongola <133748743+WakakiVongola@users.noreply.github.com>
2024-04-15 11:02:03 +0200

    Update layout.tsx added import footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from '@/src/components/header'
+import Header from '@/src/components/footer'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className="bg-blanc">
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
