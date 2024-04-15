5d43fb807c4eed85073615a632ba2a0cb9d87db2
WakakiVongola <133748743+WakakiVongola@users.noreply.github.com>
2024-04-15 11:03:29 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from '@/src/components/header'
-import Header from '@/src/components/footer'
+import Footer from '@/src/components/footer'
 
 const inter = Inter({ subsets: ["latin"] });
 
