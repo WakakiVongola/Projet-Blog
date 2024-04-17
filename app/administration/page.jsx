6aad98c0ef6ed3306874534800903083b85b4cbf
rayan <abdlkaderayan@gmail.com>
2024-04-17 13:00:20 +0200

    page admin sans css

--- app/administration/page.jsx
+++ app/administration/page.jsx
@@ -14,8 +14,8 @@ export default function Administration() {
                             width={60}
                             height={60}
                         /></a>
-                    </div>   
-                        {/* Compte administrateur */}
+                    </div>  
+                        {/* bouton dashboard */}
                     <div className="flex items-center justify-auto border border-black px-5 mr-10">
                         <h3 className="font-semibold text-marron text-center text-2xl">Compte administrateur</h3>
                     </div>
@@ -23,11 +23,11 @@ export default function Administration() {
                 <div className="w-auto h-28 border-black border-b-2">
                     <div className="flex">
                         <div className="flex items-center">
-                            <a href="/administration/utilisateurs" className="bg-beige hover:bg-beige text-white font-bold py-2 px-4 rounded flex items-center justify-center ml-64">Utilisateurs</a>
+                            <a href="/administration/utilisateurs" className="bg-beige hover:bg-beige text-white font-bold py-2 px-4 rounded flex items-center justify-center ml-64 hover:brightness-50">Utilisateurs</a>
                         </div>
                         <div className="h-28 w-1 bg-black mx-auto"></div>
                         <div className="flex items-center">
-                            <a href="/administration/articles" className="bg-beige hover:bg-beige text-white font-bold py-2 px-4 rounded flex items-center justify-center mr-72">Articles</a>
+                            <a href="/administration/articles" className="bg-beige hover:bg-beige text-white font-bold py-2 px-4 rounded flex items-center justify-center mr-72 border-4 border-white hover:brightness-50">Articles</a>
                         </div>
                     </div>
                 </div>
